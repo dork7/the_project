4ba20e9605e29e62d55646ab30433909e812fe87
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-05-07 12:36:56 +0400

    nav changes

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -5,10 +5,18 @@ const NavBar = () => {
     return (
         <nav>
             <li>
-                <Link to={`/`} className='nav-link'>Home</Link>
+                <Link to={`/`} className='nav-link'>
+                    <p>
+                        Home
+                    </p>
+                </Link>
             </li>
             <li>
-                <Link to={"/login"}>Login</Link>
+                <Link to={"/login"}>
+                    <p>
+                        Login
+                    </p>
+                </Link>
             </li>
         </nav>
     )
