dfac049fb7c2debeb032891f35f4dd237b9cf248
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-08-20 13:50:46 +0400

    reselect added for products

--- src/store/product/product.selector.ts
+++ src/store/product/product.selector.ts
@@ -1,4 +1,11 @@
-export const getProductSelector = (state => state.products.products.reduce((acc, product) => {
+import { createSelector } from 'reselect'
+
+const getProductReducer = (state) => state.products
+
+export const getProduct = createSelector([getProductReducer], (productsSlice) => productsSlice.products)
+
+
+export const getProductSelector = createSelector([getProduct], (products) => products.reduce((acc, product) => {
     const { title, items } = product
     acc[title.toLowerCase()] = items
     return acc
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,10 +4,26 @@ import logger from 'redux-logger'
 import { rootReducer } from './rootReducer'
 
 
-const middleWares = [
-    logger
-]
 
+const middleWareLogger = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+    console.log('action.type', action.type)
+    console.log('action.payload', action.payload)
+    console.log('store.getState()', store.getState())
+
+    next(action)
+
+    console.log('new state:', store.getState())
+
+}
+
+
+const middleWares = [
+    // logger,
+    middleWareLogger
+]
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
 export const store = createStore(rootReducer, undefined, composedEnhancers)
